5586b007a9f8bfced3b50bb96abeba590bf38483
Oleg <marchello3007@gmail.com>
2022-08-15 07:25:27 +0300

    add settings

---
+++ src/container/App/App.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+type Props = {}
+
+const Applist333 = () => {
+    return <p>Пробный текст</p>
+}
+
+const App = (props: Props) => {
+    return (
+        <>
+            <Applist333 />
+        </>
+    )
+}
+
+export default App
+
+// import React from 'react'
+
+// function App88() {
+//     return (
+//         <>
+//             <div className="test">
+//                 <h1> 444 Это тестовый текст блогового ресурса ДЗ</h1>
+//             </div>
+//         </>
+//     )
+// }
+
+// export default App88
